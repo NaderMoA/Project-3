c20295fc3f80720cfa59a08ee92ac7157b2feb49
jack <72477596+jack-Protst@users.noreply.github.com>
2024-02-26 19:35:31 -0500

    Update import requests SSA.py

--- import requests SSA.py
+++ import requests SSA.py
@@ -1,5 +1,5 @@
 import requests
-
+import os
 # List of URLs to download
 urls = [
     'https://www.dropbox.com/scl/fi/pf6hyz42brkn8bebw1xmn/common_cases.csv?rlkey=hgdf6a1kuee3qyfotf2vy19fv&dl=1',
@@ -10,6 +10,11 @@ urls = [
     'https://www.dropbox.com/scl/fi/iatrah21uj4km087sop1j/new_vs_death.csv?rlkey=ep9e9kvbnsusprdhxo0v8o5c3&dl=1',
     'https://www.dropbox.com/scl/fi/qmvyqbobvyymedm941jb5/trends_asr.csv?rlkey=llzy24je8w3jjlphiq7irid1e&dl=1'
 ]
+
+def extract_filename(url):
+    path = url.split('?')[0]  # Remove query string
+    filename = path.split('/')[-1]  # Extract file name
+    return filename
 
 # Loop through the URLs
 for i, url in enumerate(urls):
