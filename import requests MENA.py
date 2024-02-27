757154b56d9bbc5d7783bb7223bd2c3cd045f1b8
jack <72477596+jack-Protst@users.noreply.github.com>
2024-02-26 19:34:37 -0500

    Update import requests MENA.py

--- import requests MENA.py
+++ import requests MENA.py
@@ -1,5 +1,5 @@
 import requests
-
+import os
 # List of URLs to download
 urls = [
     'https://www.dropbox.com/scl/fi/7cjucrkyvyabumk6efi9b/cancer_by_excess.csv?rlkey=pt4kr4e1tysjodcc2kf8ojmwe&dl=1',
@@ -8,6 +8,11 @@ urls = [
     'https://www.dropbox.com/scl/fi/l5wtqx22bj6blbnzy1vq6/most_common_cancer_mf.csv?rlkey=bp9fq6kvxwtkru59qc8f6ss2h&dl=1',
     'https://www.dropbox.com/scl/fi/gdmaev7u83wmpvsnwe9zg/new_case_mf.csv?rlkey=l4m85xpj4qlwhu40uk7mmthz6&dl=1'
 ]
+
+def extract_filename(url):
+    path = url.split('?')[0]  # Remove query string
+    filename = path.split('/')[-1]  # Extract file name
+    return filename
 
 # Loop through the URLs
 for i, url in enumerate(urls):
