5f1dc41b5f1f0e75bcda9c328e94d9bd0f6f6192
NaderMoa <nadermoazezi@gmail.com>
2024-04-20 15:31:21 -0400

    argentina graph is created

--- static/js/prediction.js
+++ static/js/prediction.js
@@ -1,44 +1,65 @@
-// creating Argentina chart
 document.addEventListener('DOMContentLoaded', function() {
-    d3.json('/predictionargentina_forecast').then(function(data) {
-        const ds = data.map(item => item.ds);
-        const yhat = data.map(item => item.yhat);
+    // Fetch data for argentina_forecast
+    fetch('/predictionargentina_forecast')
+        .then(response => response.json())
+        .then(data => {
+            const argentina_ds = data.map(item => new Date(item.ds).getFullYear());
+            const argentina_yhat = data.map(item => item.yhat);
 
-        const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-        const width = 800 - margin.left - margin.right;
-        const height = 400 - margin.top - margin.bottom;
+            // Fetch data for f_argentina_forecast
+            fetch('/prediction/f_argentina_forecast')
+                .then(response => response.json())
+                .then(f_data => {
+                    const f_argentina_ds = f_data.map(item => new Date(item.ds).getFullYear());
+                    const f_argentina_yhat = f_data.map(item => item.yhat);
 
-        const svg = d3.select("#predictionChart")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
-
-        const x = d3.scaleBand()
-            .domain(ds)
-            .range([0, width])
-            .padding(0.1);
-
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(yhat)])
-            .range([height, 0]);
-
-        const line = d3.line()
-            .x((d, i) => x(ds[i]) + x.bandwidth() / 2)
-            .y(d => y(d));
-
-        svg.append("path")
-            .datum(yhat)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
-            .attr("d", line);
-
-        svg.append("g")
-            .attr("transform", `translate(0,${height})`)
-            .call(d3.axisBottom(x));
-
-        svg.append("g")
-            .call(d3.axisLeft(y));
-    });
+                    // Chart.js configuration
+                    const ctx = document.getElementById('predictionChart').getContext('2d');
+                    const chart = new Chart(ctx, {
+                        type: 'line',
+                        data: {
+                            labels: argentina_ds,
+                            datasets: [
+                                {
+                                    label: 'Men',
+                                    data: argentina_yhat,
+                                    borderColor: 'steelblue',
+                                    fill: false
+                                },
+                                {
+                                    label: 'Women',
+                                    data: f_argentina_yhat,
+                                    borderColor: 'orange',
+                                    fill: false
+                                }
+                            ]
+                        },
+                        options: {
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Lung Cancer Death Rate', // Title for the entire chart
+                                    font: {
+                                        size: 18
+                                    }
+                                }
+                            },
+                            scales: {
+                                x: {
+                                    title: {
+                                        display: true,
+                                        text: 'Year'
+                                    }
+                                },
+                                y: {
+                                    title: {
+                                        display: true,
+                                        text: 'Prediction'
+                                    }
+                                }
+                            }
+                        }
+                    });
+                });
+        });
 });
